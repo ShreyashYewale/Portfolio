d83898a9735405344167f3babdd8dcd17215b2a3
shreyash <shreyashyewale84964@gmail.com>
2021-06-25 15:25:23 +0530

    Simple change

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    title: "React Reserve",
+    title: "Quadcopter Using Arduino",
     subtitle: "MERN Stack",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
